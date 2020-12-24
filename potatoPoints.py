152f7da9a869088dcced3e187d25ddbe1d2b6529
thetateman <tdsmith2018@gmail.com>
2020-12-24 02:52:01 -0600

    appears to calculate trick winner correctly

--- potatoPoints.py
+++ potatoPoints.py
@@ -26,13 +26,13 @@ def initialize_deck():
 
 def shuffle_deck(deck):
     shuffled_deck_x = []
-    index = 0
+    index_h = 0
     for b in range(0, len(deck)):
-        pick_card = random.randint(0, 39 - index)
+        pick_card = random.randint(0, 39 - index_h)
         # print("testing...pick_card:  " + str(pick_card))
         shuffled_deck_x.append(deck[pick_card])
         deck.pop(pick_card)
-        index = index + 1
+        index_h = index_h + 1
     return shuffled_deck_x
 
 
@@ -56,10 +56,17 @@ p4.hand.extend(shuffled_deck[30:35])
 p4.field.extend(shuffled_deck[35:40])
 
 players = [p1, p2, p3, p4]
+lastRoundWinner = 0
 print("Hello, welcome to PotatoPoints!\nLicensed under GPL (GNU General Public License) in 2020 by Tate Smith.\n\n")
 for trickIndex in range(10):
-    turnIndex = 0
-    while turnIndex <= 3:
+    realOrderCounter = 0
+    turnIndex = lastRoundWinner
+    trick = []
+    trickColor = ""
+    leadingSuit = ""
+    playerWinningTrick = turnIndex
+    winningCard = 0
+    while realOrderCounter <= 3:
         print("\nPlayer 1's Field: ")
         for r in range(len(p1.field)):
             print(str(p1.field[r].value) + str(p1.field[r].suit), end=" ")
@@ -90,20 +97,105 @@ for trickIndex in range(10):
             print("please input in the correct format")
             continue
         cardPlayable = False
+        otherEligibleCard = False
+        tryAgain = False
         for handIter in players[turnIndex].hand:
             if (handIter.value == moveValue) and (handIter.suit == moveSuit):
+                if not (realOrderCounter == 0):
+                    if (moveSuit == "h" or moveSuit == "d") and (trickColor == "black"):
+                        for handIterSecond in players[turnIndex].hand:
+                            if handIterSecond.suit == "c" or handIterSecond.suit == "s":
+                                otherEligibleCard = True
+                                break
+                        for fieldIterSecond in players[turnIndex].field:
+                            if fieldIterSecond.suit == "c" or fieldIterSecond.suit == "s":
+                                otherEligibleCard = True
+                                break
+                    if (moveSuit == 'c' or moveSuit == "s") and (trickColor == "red"):
+                        for handIterSecond in players[turnIndex].hand:
+                            if handIterSecond.suit == "h" or handIterSecond.suit == "d":
+                                otherEligibleCard = True
+                                break
+                        for fieldIterSecond in players[turnIndex].field:
+                            if fieldIterSecond.suit == "h" or fieldIterSecond.suit == "d":
+                                otherEligibleCard = True
+                                break
+                    if otherEligibleCard:
+                        print("You must follow color if you are able.")
+                        tryAgain = True
+                        break
+
                 cardPlayable = True
+                trick.append(handIter)
                 players[turnIndex].hand.remove(handIter)
                 break
         for fieldIter in players[turnIndex].field:
             if (fieldIter.value == moveValue) and (fieldIter.suit == moveSuit):
+                if not (realOrderCounter == 0):
+                    if (moveSuit == "h" or moveSuit == "d") and (trickColor == "black"):
+                        for handIterSecond in players[turnIndex].hand:
+                            if handIterSecond.suit == "c" or handIterSecond.suit == "s":
+                                otherEligibleCard = True
+                                break
+                        for fieldIterSecond in players[turnIndex].field:
+                            if fieldIterSecond.suit == "c" or fieldIterSecond.suit == "s":
+                                otherEligibleCard = True
+                                break
+                    if (moveSuit == 'c' or moveSuit == "s") and (trickColor == "red"):
+                        for handIterSecond in players[turnIndex].hand:
+                            if handIterSecond.suit == "h" or handIterSecond.suit == "d":
+                                otherEligibleCard = True
+                                break
+                        for fieldIterSecond in players[turnIndex].field:
+                            if fieldIterSecond.suit == "h" or fieldIterSecond.suit == "d":
+                                otherEligibleCard = True
+                                break
+                    if otherEligibleCard:
+                        print("You must follow color if you are able.")
+                        tryAgain = True
+                        break
+
                 cardPlayable = True
+                trick.append(fieldIter)
                 players[turnIndex].field.remove(fieldIter)
                 break
+        if tryAgain:
+            continue
         if not cardPlayable:
             print("you don't have that card, try again.")
             continue
+
+        if realOrderCounter == 0:
+            leadingSuit = trick[0].suit
+            if leadingSuit == "h" or leadingSuit == "d":
+                trickColor = "red"
+            if leadingSuit == "c" or leadingSuit == "s":
+                trickColor = "black"
+        else:
+            if moveValue > trick[winningCard].value:
+                if ((moveSuit == "c" or moveSuit == "s") and (trickColor == "black")) or (
+                        (moveSuit == "h" or moveSuit == "d") and (trickColor == "red")):
+                    winningCard = realOrderCounter
+                    playerWinningTrick = turnIndex
+                    print(str(turnIndex) + " is now winning the trick")
+            elif moveValue == trick[winningCard].value:
+                if moveSuit == leadingSuit:
+                    winningCard = realOrderCounter
+                    playerWinningTrick = turnIndex
+                    print(str(turnIndex) + " is now winning the trick.")
+
         turnIndex = turnIndex + 1
-        # print(str(moveValue) + str(moveSuit))
+        if turnIndex == 4:
+            turnIndex = 0
+        realOrderCounter = realOrderCounter + 1
+    trickSum = 0
+    for playedCard in trick:
+        if playedCard.value % 2 == 0:
+            trickSum = trickSum + playedCard.value
+    players[playerWinningTrick].points = players[playerWinningTrick].points + trickSum
+    for index in range(4):
+        print("Player " + str(index) + ": " + str(players[index].points))
+
+    # print(str(moveValue) + str(moveSuit))
 
 exit(0)
